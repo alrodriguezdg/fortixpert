28eb261b50145a659db8aee6821b4ccf3bb88392
Alejandro <alrodriguezdg@gmail.com>
2019-06-13 07:36:45 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Child Xtocky Theme
+  Recommended way to include parent theme styles.
+  Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme
+*/
+
+/**
+ * Enqueue style of child theme
+ */
+
